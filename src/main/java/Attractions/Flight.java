9242d8435e331d529b832d14b969921d395e8f97
Danny Welsh <dannywelsh400@hotmail.com>
2019-06-04 22:19:49 +0100

    Introduces the ISecurity Interface and Implements it in relevant classes

--- src/main/java/Attractions/Flight.java
+++ src/main/java/Attractions/Flight.java
@@ -1,8 +1,15 @@
 package Attractions;
 
-public class Flight extends Attraction {
+import AltJurassicPark.ISecurity;
+import AltJurassicPark.Visitor;
+
+public class Flight extends Attraction implements ISecurity {
 
     public Flight(){
         super("Pterodactyl Rides");
+    }
+
+    public boolean isAllowedTo(Visitor visitor) {
+        return ((visitor.getAge() >= 14) && (visitor.getHeight() <= 180));
     }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,20 +1,43 @@
+import AltJurassicPark.Visitor;
 import Attractions.Flight;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 
 public class FlightTest {
 
     private Flight flight;
+    private Visitor visitor1;
+    private Visitor visitor2;
+    private Visitor visitor3;
 
     @Before
     public void setUp() {
         flight = new Flight();
+        visitor1 = new Visitor(20, 175, 200);
+        visitor2 = new Visitor(13, 140, 150);
+        visitor3 = new Visitor(19, 185, 140);
     }
 
     @Test
     public void hasName() {
         assertEquals("Pterodactyl Rides", flight.getName());
+    }
+
+    @Test
+    public void isAllowedToFlyAPterodactyl() {
+        assertTrue(flight.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void isNotAllowedToFlyAPterodactyl__TooYoung() {
+        assertFalse(flight.isAllowedTo(visitor2));
+    }
+
+    @Test
+    public void isNotAllowedToFlyAPterodactyl__TooTall() {
+        assertFalse(flight .isAllowedTo(visitor3));
     }
 }
